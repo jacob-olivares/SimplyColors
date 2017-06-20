7be42c8d47fee2e352fdbed7995290b85237801b
jhaco <jhaco@DESKTOP-G2DC0EK>
2017-06-20 09:11:21 -0400

    estilos

--- index.php
+++ index.php
@@ -4,9 +4,11 @@
 <html>
     <head>
         <meta charset="UTF-8">
+        <link href="css/estilos.css" rel="stylesheet" type="text/css"/>
         <title></title>
     </head>
     <body>
+        
         <?php if(isset($_SESSION['USR'])) { ?>
             <a href="./model/agregarUsuario.php">Agregar Usuario</a>
             <a href="./model/modificarUsuario.php">Modificar Usuario</a>
@@ -14,11 +16,14 @@
             <a href="cerrar.php">Cerrar Sesion</a>
         <?php } ?> 
         <?php if(!isset($_SESSION['USR'])) { ?>
+        <div id="login">
+        <h1>Mantenedor de usuarios: </h1>
         <form action="login.php" method="POST">
             <div>User: <input type="text" name="user"></div>
             <div>Password: <input type="password" name="pass"></div>
         <input type="submit" value="Login">
         </form>
+        </div>
         <?php
          } 
         ?>
