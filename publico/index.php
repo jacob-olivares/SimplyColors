a56ca318e83b46aace57315c28321568adbea84c
cetecom <cetecom@LC08L1203>
2017-06-22 13:48:30 -0400

    sitio publico

---
+++ publico/index.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link href="css/estilos.css" rel="stylesheet" type="text/css"/>
+        <title></title>
+    </head>
+    <body>
+        <img src="img/logo_simply_colors.png" alt=""/>
+        <div id="contenedor">
+            
+        </div>
+    </body>
+</html>
+
