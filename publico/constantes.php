59423867de6e1fefab14cd57e28db33e3ecb8062
jhaco <jhaco@DESKTOP-G2DC0EK>
2017-06-29 00:56:40 -0400

    Avance en portal publico

---
+++ publico/constantes.php
@@ -0,0 +1,6 @@
+<?php
+define('PATH',$_SERVER["DOCUMENT_ROOT"]."/SimplyColors/publico");
+define('TITPAG',"simply colors | Regalo para bebés y niños - Camisetas y Ropa personalizada con nombre");
+session_start();
+
+
--- publico/index.php
+++ publico/index.php
@@ -1,12 +1,82 @@
+<?php
+    include './constantes.php';
+?>
 <html>
     <head>
         <meta charset="UTF-8">
-        <link href="css/estilos.css" rel="stylesheet" type="text/css"/>
-        <title></title>
+        <link href="css/estilos.css?v=<?=time()?>" rel="stylesheet" type="text/css"/>
+        <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+        <title><?=TITPAG?></title>
     </head>
     <body>
-        <img src="img/logo_simply_colors.png" alt=""/>
+        <div id="barraSuperior">
+            <img src="img/logo_simply_colors.png" alt=""/>
+        </div>
+        <input id="buscaProducto" type="text" name="buscaProducto" value="Busca tu producto...">
+        <div id="menu">
+            <ul>
+                <li class>
+                <i class="fa fa-home" aria-hidden="false"></i>
+                </li>
+                <li class>
+                    <a href="bebe.php" title="bebe | simplycolors">bebe</a>
+                </li>
+                                
+                <li class>
+                    <a href="ninos.php" title="niñ@s | simplycolors">niñ@s</a>
+                </li>
+                                
+                <li class>
+                    <a href="adulto.php" title="adulto | simplycolors">adulto</a>
+                </li>
+                                
+                <li class>
+                    <a href="casa.php" title="casa | simplycolors">casa</a>
+                </li>
+                                
+                <li class>
+                    <a href="cole.php" title="al cole | simplycolors">al cole</a>
+                </li>
+                                
+                <li class>
+                    <a href="regalo.php" title="regalo | simplycolors">regalo</a>
+                </li>
+                                
+                <li class>
+                    <a href="momentos.php" title="momentos | simplycolors">momentos</a>
+                </li>
+                                
+                <li class>
+                    <a href="profesores.php" title="Profesores | simplycolors">Profesores</a>
+                </li>
+            </ul>
+            <div id="barraInferior">
+                <div id="barraInfCont">
+                    <div class="contBarraInf">
+                        <table>
+                            <tr>
+                                <td><i class="fa fa-check"></i>&nbsp;Ropa y accesorios personalizados</td>
+                                <td><i class="fa fa-check"></i>&nbsp;envio gratis a partir de 40 euros</td>
+                                <td><i class="fa fa-check"></i>&nbsp;excelente calidad y servicio</td>
+                                <td><i class="fa fa-check"></i>&nbsp;el regalo perfecto</td>
+                            </tr>
+                        </table>
+                    </div>
+                </div>
+            </div>
+            <div class="barraCentral">
+                <div class="op">
+                    <div class="opCont">
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+
         <div id="contenedor">
+            <div id="contImg">
+                <img src="img/flechas-bodies-personalizados-crea-tu-diseno.png" alt=""/>
+            </div>
             
         </div>
     </body>
