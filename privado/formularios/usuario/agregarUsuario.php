1319e04f15f186b161fe802ba2b90adc4472af3e
cetecom <cetecom@LC08L1201>
2017-06-29 13:03:36 -0400

    Css usuario

--- privado/formularios/usuario/agregarUsuario.php
+++ privado/formularios/usuario/agregarUsuario.php
@@ -1,18 +1,43 @@
+
+<?php
+    include '../../Constantes.php';
+    include '../../Librerias.php';
+    if(isset($_SESSION['USR'])) {
+    
+    
+?>
 <html>
     <head>
         <meta charset="UTF-8">
         <title></title>
+          <link href="../../css/estilos.css" rel="stylesheet" type="text/css"/>
     </head>
     <body>
-         
+         <div id="Cabecera">
+             
+            
+            <img src="../../../publico/img/logo_simply_colors.png" alt=""/>
+        </div>
+        <div align="right"><button><a id="cancelar" href="../../index.php">Cancelar</a></button></div>    
+        <div id="Cuerpo">
+            <h1>Agregar Usuario!</h1>
+            <div id="agregarUsu">
         <form action="../../controladores/usuario/agregar.php" method="POST">
-            <div>Usuario: <input type="text" name="user"></div>
-            <div>Password: <input type="password" name="pass"></div>
+            <div>Usuario:</div><input type="text" name="user">
+            <div>Password: </div><input type="password" name="pass">
             <div>Confirma Password: <input type="password" name="confirm-pass"></div>
             <div>Nombre Usuario: <input type="text" name="nombre"></div>
             <div>Apellido Usuario: <input type="text" name="apellido"></div>
         <input type="submit" value="Enviar"> 
         </form>
-                     
+        </div>
+            </div> 
+      
+    </body>
 </html>
 
+<?php } ?>
+<?php
+if (!isset($_SESSION['USR'])) {
+    header('Location:http://localhost:' . $_SERVER['SERVER_PORT'] . '/SimplyColors/privado/index.php');
+}?>
--- privado/formularios/usuario/modificarUsuario.php
+++ privado/formularios/usuario/modificarUsuario.php
@@ -1,17 +1,41 @@
+
+<?php
+    include '../../Constantes.php';
+    include '../../Librerias.php';
+    if(isset($_SESSION['USR'])) {
+    
+  
+?>
 <html>
     <head>
         <meta charset="UTF-8">
         <title></title>
+          <link href="../../css/estilos.css?v=<?= time(); ?>" rel="stylesheet" type="text/css"/>
     </head>
     <body>
-        <form action="../../controladores/usuario/modificar.php" method="POST">
-            <div>User: <input type="text" name="user"></div>
-            <div>Password: <input type="password" name="pass"></div>
-            <div>New Password: <input type="password" name="newpass"></div>
-            <div>Nombre: <input type="text" name="nombre"></div>
-            <div>Apellido: <input type="text" name="apellido"></div>
+         <div id="Cabecera"> 
+            <img src="../../../publico/img/logo_simply_colors.png" alt=""/>
+        </div>
+        <div align="right"><button><a id="cancelar" href="../../index.php">Cancelar</a></button></div>    
+        <div id="Cuerpo">
+            <h1>Modificar Usuario!</h1>
+         <div id="modificarUsu">
+         <form action="../../controladores/usuario/modificar.php" method="POST">
+            <div>User: </div><input type="text" name="user">
+            <div>Password:</div> <input type="password" name="pass">
+            <div>New Password:</div> <input type="password" name="newpass">
+            <div>Nombre: </div><input type="text" name="nombre">
+            <div>Apellido: </div><input type="text" name="apellido">
             <input type="submit" value="Modify">
         </form>
+        </div>
+            </div> 
+      
     </body>
 </html>
 
+<?php } ?>
+<?php
+if (!isset($_SESSION['USR'])) {
+    header('Location:http://localhost:' . $_SERVER['SERVER_PORT'] . '/SimplyColors/privado/index.php');
+}?>
